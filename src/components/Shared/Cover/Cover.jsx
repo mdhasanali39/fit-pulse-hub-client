32baf0871dcd3b1515609c11520701ba5207c4b4
mdhasanali39 <contact.hasankhanyt@gmail.com>
2023-11-26 09:13:50 +0600

    Cover: created cover component for small banner in which page I want, and this cover component content will be dynamic

---
+++ src/components/Shared/Cover/Cover.jsx
@@ -0,0 +1,16 @@
+import Title from "../Title/Title";
+
+const Cover = ({heading_first,heading_last,subHeading,image,color}) => {
+  return (
+    <div style={{background: `linear-gradient(83deg, rgba(35,35,36,0.5662640056022409) 100%, rgba(112,92,9,0) 100%),URL(${image})`, backgroundSize: 'cover', backgroundRepeat:'no-repeat,no-repeat', backgroundPosition:'top'}} className="h-[400px] flex justify-center items-center ">
+      <Title
+        heading_first={heading_first}
+        heading_last={heading_last}
+        color={color}
+        subHeading={subHeading}
+      ></Title>
+    </div>
+  );
+};
+
+export default Cover;
